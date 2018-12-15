f12025f3fd511c79e8f41011b6699401c451eb9f
Alberto Turégano Castedo <alberto_tureganocsb@hotmail.com>
2018-12-15 19:25:12 +0100

    Add files via upload

---
+++ P2/syscall.c
@@ -0,0 +1,16 @@
+#include <linux/errno.h>
+#include <sys/syscall.h>
+#include <linux/unistd.h>
+#include <stdio.h>
+#ifdef __i386__
+#define __NR_HELLO    383
+#else
+#define __NR_HELLO    332
+#endif
+
+long lin_hello(void) {
+   return (long) syscall(__NR_HELLO);
+}
+int main(void) {
+      return lin_hello();
+}
